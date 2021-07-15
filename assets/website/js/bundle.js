d369bd9cf91df4bb8f7be48a9eb11d79f34a33a7
Francisco Luis Rios Vega <franciscorios0610@gmail.com>
2021-07-14 20:49:34 -0500

    webpack encore

---
+++ assets/website/js/bundle.js
@@ -0,0 +1,3 @@
+import "../scss/bundle.scss";
+
+console.log("App Ready");
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AdminController extends AbstractController
+{
+  private $data;
+
+  function __construct() 
+  {
+      $this->data = [];
+  }
+
+  public function index()
+  {
+    return $this->render("admin/layout.html.twig", $this->data);
+  }
+}
