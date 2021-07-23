306b20321835f8980a1cefc9a73cf67bf2a624b9
Francisco Luis Rios Vega <franciscorios0610@gmail.com>
2021-07-22 20:46:43 -0500

    Encore multiple config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require("@symfony/webpack-encore");
+const dotenv = require("dotenv");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -6,83 +7,71 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
-Encore
-  // directory where compiled assets will be stored
-  .setOutputPath("public/build/")
-  // public path used by the web server to access the output path
-  .setPublicPath("/build")
-  // only needed for CDN's or sub-directory deploy
-  //.setManifestKeyPrefix('build/')
-
-  // enables copy images
+// Define the website configuration
+Encore.setOutputPath("public/build/website")
+  .setPublicPath("/build/website")
+  .addEntry("bundle", "./assets/website/js/bundle.js")
   .copyFiles({
     from: "./assets/website/images",
-    // optional target path, relative to the output dir
-    to: "website/images/[path][name].[ext]",
-
-    // if versioning is enabled, add the file hash too
-    //to: 'images/[path][name].[hash:8].[ext]',
-
-    // only copy files matching this pattern
+    to: "images/[path][name].[ext]",
     pattern: /\.(png|jpg|jpeg|mp4)$/,
   })
-
-  /*
-   * ENTRY CONFIG
-   *
-   * Each entry will result in one JavaScript file (e.g. app.js)
-   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-   */
-  .addEntry("app_bundle", "./assets/website/js/bundle.js")
-  .addEntry("cms_bundle", "./assets/admin/js/bundle.js")
-
-  // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-  // .enableStimulusBridge('./assets/controllers.json')
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
-
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
   .enableSingleRuntimeChunk()
-
-  /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
-   */
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
   .enableSourceMaps(!Encore.isProduction())
-  // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(Encore.isProduction())
-
   .configureBabel((config) => {
     config.plugins.push("@babel/plugin-proposal-class-properties");
   })
-
-  // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
     config.useBuiltIns = "usage";
     config.corejs = 3;
   })
+  .enableSassLoader();
 
-  // enables Sass/SCSS support
+// Build the website configuration
+const websiteConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the website configuration
+websiteConfig.name = "websiteConfig";
+
+// Resert Encore to build the admin configuration
+Encore.reset();
+
+///// afsafasfs
+Encore.setOutputPath("public/build/admin")
+  .setPublicPath("/build/admin")
+  .addEntry("bundle", "./assets/admin/js/bundle.js")
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  // .configureDefinePlugin((config) => {
+  //   const env = dotenv.config();
+
+  //   if (env.error) throw env.error;
+
+  //   config["process.env"].API_URL = JSON.stringify(env.parsed.API_URL);
+  // })
+  .configureBabel((config) => {
+    config.plugins.push("@babel/plugin-proposal-class-properties");
+  })
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = "usage";
+    config.corejs = 3;
+  })
   .enableSassLoader()
-
-  // uncomment if you use TypeScript
-  //.enableTypeScriptLoader()
-
-  // uncomment if you use React
   .enableReactPreset();
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
+// Build the admin configuration
+const adminConfig = Encore.getWebpackConfig();
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+// Set a unique name for the admin configuration
+adminConfig.name = "adminConfig";
 
-module.exports = Encore.getWebpackConfig();
+// Export the final configuration as an array of multiple configurations
+module.exports = [websiteConfig, adminConfig];
